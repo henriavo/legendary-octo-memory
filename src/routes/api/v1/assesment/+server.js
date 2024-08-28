0243591116dba31356c583109a7badc6878aac3e
Henri Idrovo <3028339+henriavo@users.noreply.github.com>
2024-08-28 09:48:07 -0500

    read json

--- src/routes/api/v1/assesment/+server.js
+++ src/routes/api/v1/assesment/+server.js
@@ -1,7 +1,13 @@
 import { error } from '@sveltejs/kit';
 
-/** @type {import('./$types').RequestHandler} */ export function GET({ url }) {
-	console.log(url);
+/** @type {import('./$types').RequestHandler} */
+export async function POST({ request }) {
+	// Read the JSON data from the request body
+	const data = await request.json();
+
+	// Log the received data
+	console.log('Received JSON data:', data);
+
 	const random = 10 + Math.random() * 4;
 	return new Response(String(random));
 }
