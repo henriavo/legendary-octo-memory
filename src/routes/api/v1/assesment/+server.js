bf59bc93e36faac3de5369eb7b2c54c9c5f4581a
Henri Idrovo <3028339+henriavo@users.noreply.github.com>
2024-08-28 13:31:53 -0500

    api response success

--- src/routes/api/v1/assesment/+server.js
+++ src/routes/api/v1/assesment/+server.js
@@ -2,12 +2,100 @@ import { error } from '@sveltejs/kit';
 
 /** @type {import('./$types').RequestHandler} */
 export async function POST({ request }) {
-	// Read the JSON data from the request body
 	const data = await request.json();
+	let answers = data.answers;
 
-	// Log the received data
-	console.log('Received JSON data:', data);
+	let totalScores = caculateScores(answers);
+	let response = translateScoresToResults(totalScores);
 
-	const random = 10 + Math.random() * 4;
-	return new Response(String(random));
+	return new Response(JSON.stringify(response));
 }
+
+function caculateScores(answers) {
+	let totalScores = { depression: 0, mania: 0, anxiety: 0, substance_use: 0 };
+
+	for (let i = 0; i < answers.length; i++) {
+		let obj = answers[i];
+		//patient answers
+		// console.log('question_id=' + obj.question_id);
+		// console.log('value=' + obj.value);
+
+		//use question id to get domain
+		const question = questionToDomainMapping.find((q) => q.question_id === obj.question_id);
+		if (!question) {
+			return error(400, 'Invalid question_id');
+		}
+		// use domain to update total scores
+		totalScores[question.domain] += obj.value;
+	}
+	console.log(totalScores);
+	return totalScores;
+}
+
+
+function translateScoresToResults(totalScores) {
+	let response = {
+		results: []
+	};
+
+	const scoreThresholds = {
+		depression: 2,
+		mania: 2,
+		anxiety: 2,
+		substance_use: 1
+	};
+
+	const domainToAssesment = {
+		depression: 'PHQ-9',
+		mania: 'ASRM',
+		anxiety: 'PHQ-9',
+		substance_use: 'ASSIST'
+	};
+
+	for (const domain in totalScores) {
+		const score = totalScores[domain];
+		const threshold = scoreThresholds[domain];
+		if (score >= threshold) {
+			let assesment = domainToAssesment[domain];
+			response.results.push(assesment);
+		}
+	}
+
+	return response;
+}
+
+// a constant. in theory this would be accessed from a database
+const questionToDomainMapping = [
+	{
+		question_id: 'question_a',
+		domain: 'depression'
+	},
+	{
+		question_id: 'question_b',
+		domain: 'depression'
+	},
+	{
+		question_id: 'question_c',
+		domain: 'mania'
+	},
+	{
+		question_id: 'question_d',
+		domain: 'mania'
+	},
+	{
+		question_id: 'question_e',
+		domain: 'anxiety'
+	},
+	{
+		question_id: 'question_f',
+		domain: 'anxiety'
+	},
+	{
+		question_id: 'question_g',
+		domain: 'anxiety'
+	},
+	{
+		question_id: 'question_h',
+		domain: 'substance_use'
+	}
+];
